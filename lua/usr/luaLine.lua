a45c6082eaec38b78730db12a30ff1737197e009
Zeniku <zjuliuscorpuz@gmail.com>
2022-04-06 11:23:40 +0800

    stuff

--- lua/usr/luaLine.lua
+++ lua/usr/luaLine.lua
@@ -7,10 +7,30 @@ local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
 
-local lsp_diagnostics = {
-	"lsp_diagnostics", -- custom component evil line lsp client + lualine diagnostics
+-- From evil line example i think
+local lspcl = {
+  function ()
+    -- stolen ._.
+    local msg = 'Idle'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+         return client.name
+      end
+    end
+	return msg
+  end,
+  icon = ' LSP:'
+}
+
+local diagnostics = {
+	"diagnostics",
 	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
 	symbols = { error = " ", warn = " " },
 	colored = true,
 	update_in_insert = true
@@ -30,7 +50,7 @@ local branch = {
 }
 
 local filename = {
-	"Ifilename", -- custom component combime filename and filetype which is bad code 
+	"Ifilename", -- custom component combime filename and filetype which is bad code --
 	colored = true,
 	file_status = true,
 	path = 0,
@@ -42,37 +62,33 @@ local filename = {
 	},
 }
 
--- cool function for progress 
--- stolen :c 
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
-
+-- cool function for progress --
 local location = {
 	"location",
 	fmt = function(str)
-		return str .. " " .. progress()
+		--stolen :c
+		local current_line = vim.fn.line(".")
+		local total_lines = vim.fn.line("$")
+		local chars = { "_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
+		local line_ratio = current_line / total_lines
+		local index = math.ceil(line_ratio * #chars)
+
+		return str .. " " .. chars[index]
 	end
 }
-
 
 lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "onedark",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { lsp_diagnostics },
+		lualine_b = { lspcl, diagnostics },
 		lualine_c = { filename },
 		-- lualine_x = { diff, "encoding"},
 	  lualine_x = { diff },
