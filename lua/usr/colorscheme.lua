c198296fa374633427ca8a68c9938757dd843613
Zeniku <zjuliuscorpuz@gmail.com>
2022-09-04 13:41:04 +0800

    stuff

--- lua/usr/colorscheme.lua
+++ lua/usr/colorscheme.lua
@@ -3,11 +3,6 @@ if not status_ok then
   return
 end
 
-local redgui = {
-  { fg = "$red", fmt = "bold"},
-  { fg = "$red"}
-}
-
 local conf = {
   -- Main options --
   style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
@@ -18,7 +13,7 @@ local conf = {
   toggle_style_key = '<leader>ts', -- Default keybinding to toggle
   -- toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
   toggle_style_list = { 'deep', 'darker' },
-  -- Change code style
+  -- Change code style ---
   -- Options are italic, bold, underline, none
   -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
   code_style = {
@@ -34,12 +29,17 @@ local conf = {
     darkgreen = "#679B49"
   }, -- Override default colors
   highlights = {
-    javaAccessKeyword = redgui[0], -- install uiiaoo/java-syntax.vim
-    javaInclude = redgui[0],
-    Conditional = redgui[1],
-    Statement = redgui[1],
-    Exeption = redgui[1],
-    Repeat = redgui[1],
+    -- install uiiaoo/java-syntax.vim
+    javaAccessKeyword = { fg = "$red", fmt = "bold"},
+    javaInclude = { fg = "$red", fmt = "bold"},
+    javaIdentifier = { fmt = "bold"},
+    javaPackagePath = {fg = "$yellow"},
+    javaPackageName = { fg = '$red'},
+    -- Default 
+    Conditional = { fg = "$red"},
+    Statement = { fg = "$red"},
+    Exeption = { fg = "$red"},
+    Repeat = { fg = "$red"},
     PreProc = { fg = '$blue'},
     Structure = { fmt = 'bold'},
     String = { fg = '$darkgreen'}
@@ -53,6 +53,13 @@ local conf = {
   },
 }
 
+vim.cmd [[
+  syntax match javaPackageName "\v(^package\s+)@<=.*"
+  syntax match javaimportName "\v(^import\s+)@<=.*"
+  hi! link javaPackageName Include
+  hi! link javaImportName javaPackagePath
+]]
+
 local colors = require("onedark.palette")[conf.style]
 -- Lua
 onedark.setup(conf)
@@ -65,6 +72,3 @@ vim.g.rainbow_conf = {
 }
 
 onedark.load()
-
--- Disable highlight most of stuff in java syntax
-vim.cmd[[ hi link javaIdentifier None ]]
--- lua/usr/lsp/lsp-installer.lua
+++ lua/usr/lsp/lsp-installer.lua
@@ -2,32 +2,22 @@ local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
 	return
 end
+local lspconfig = require("lspconfig")
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
+local servers = { "jsonls", "jdtls" , "tsserver"}
+
+lsp_installer.setup({
+	ensure_installed = servers,
+})
+
+for _, server in pairs(servers) do
 	local opts = {
 		on_attach = require("usr.lsp.handlers").on_attach,
-		capabilities = require("usr.lsp.handlers").capabilities
+		capabilities = require("usr.lsp.handlers").capabilities,
 	}
-
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("usr.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
-	 if server.name == "jdtls" then
-	 	local jdtls_opts = require("usr.lsp.settings.jdtls")
-	 	opts = vim.tbl_deep_extend("force", jdtls_opts, opts)
-	 end
-
-	 --if server.name == "pyright" then
-	 	--local pyright_opts = require("lsp.settings.pyright")
-	 	--opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 --end
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
+	local has_custom_opts, server_custom_opts = pcall(require, "usr.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
+	lspconfig[server].setup(opts)
 end
-)
